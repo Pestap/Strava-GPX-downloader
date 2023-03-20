1f8f23399049131cdd24134231ed2989f1272499
Pestap <90579181+Pestap@users.noreply.github.com>
2023-03-21 00:20:37 +0100

    Started work on activity download

--- main.py
+++ main.py
@@ -18,4 +18,12 @@ def get_athlete() -> str:
     return requests.get(athlete_url).json()
 
 
-print(get_athlete()['firstname'])
+# get list of activities in json format
+def get_activities_of_type(activity_type: str) -> [str]:
+    access_token = load_tokens('strava_tokens.json')[0]
+    activities_url = f"https://www.strava.com/api/v3/athlete/activities?" \
+                  f"access_token={access_token}"
+
+    return requests.get(activities_url).json()
+
+print(get_activities_of_type("asd"))
