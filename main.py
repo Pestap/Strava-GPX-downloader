e6f85b955dada87352bc516e2fabeed44a588f21
Pestap <90579181+Pestap@users.noreply.github.com>
2023-03-21 00:08:30 +0100

    Added athlete information download

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ import requests
 def load_tokens(filename: str) -> (str,str):
     with open(filename, 'r') as file:
         tokens = json.load(file)
-        return tokens['access_token'],tokens['refresh_token']
+        return tokens['access_token'], tokens['refresh_token']
 
 
+# Get athlete info in json format
+def get_athlete() -> str:
+    access_token = load_tokens('strava_tokens.json')[0]
+    athlete_url = f"https://www.strava.com/api/v3/athlete?" \
+                  f"access_token={access_token}"
 
+    return requests.get(athlete_url).json()
+
+
+print(get_athlete()['firstname'])
