8bd25f84905cb1d7b852320c8ec3fe04757fd9c8
Pestap <90579181+Pestap@users.noreply.github.com>
2023-03-23 01:00:35 +0100

    Strava token fetching and refreshing in stravaconnections module

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 import json
 import requests
-
-
-# load Strava API tokens from file
-def load_tokens(filename: str) -> (str,str):
-    with open(filename, 'r') as file:
-        tokens = json.load(file)
-        return tokens['access_token'], tokens['refresh_token']
-
-
-# Get athlete info in json format
-def get_athlete() -> str:
-    access_token = load_tokens('strava_data.json')[0]
-    athlete_url = f"https://www.strava.com/api/v3/athlete?" \
-                  f"access_token={access_token}"
-
-    return requests.get(athlete_url).json()
+from stravaconnections import StravaConnection
 
 
 # get list of activities in json format
@@ -26,4 +11,5 @@ def get_activities_of_type(activity_type: str) -> [str]:
     # TODO: authorization (https://www.grace-dev.com/python-apis/strava-api/)
     return requests.get(activities_url).json()
 
-print(get_activities_of_type("asd"))
+a = StravaConnection()
+a.authorize()
--- stravaconnections.py
+++ stravaconnections.py
@@ -1,19 +1,60 @@
 import json
 
+import requests
 
 
 class StravaConnection:
     def __init__(self):
-        self.client_id = None
-        self.client_secret = None
+        self.__client_id = None
+        self.__client_secret = None
+        self.redirect_uri = 'https://localhost/'
         self.init_strava_app_data()
-
 
     # Load data from strava_data file (sensitive data)
     def init_strava_app_data(self):
         with open('strava_data.json', 'r') as file:
             strava_app_data = json.load(file)
-            self.client_id = strava_app_data['client_id']
-            self.client_secret = strava_app_data['client_secret']
+            self.__client_id = strava_app_data['client_id']
+            self.__client_secret = strava_app_data['client_secret']
 
+    def authorize(self):
+        request_url = f'https://www.strava.com/oauth/authorize?client_id={self.__client_id}' \
+                      f'&response_type=code&redirect_uri={self.redirect_uri}' \
+                      f'&approval_prompt=force' \
+                      f'&scope=profile:read_all,activity:read_all'
 
+        print("Click here to allow the app to gather your data: " + request_url)
+        code = input("Please enter the code visible in your browser: ")
+
+        token = self.request_token(code)
+        # TODO: check if valid
+        self.save_token_to_file(token)
+
+    def request_token(self, code):
+        response = requests.post(url='https://www.strava.com/api/v3/oauth/token',
+                              data={'client_id': self.__client_id,
+                                    'client_secret': self.__client_secret,
+                                    'code': code,
+                                    'grant_type': 'authorization_code'})
+        return response
+
+    def refresh_token(self):
+        token = self.read_token_from_file()
+        refresh_token = token['refresh_token']
+        response = requests.post(url='https://www.strava.com/api/v3/oauth/token',
+                                 data={'client_id': self.__client_id,
+                                       'client_secret': self.__client_secret,
+                                       'grant_type': 'refresh_token',
+                                       'refresh_token': refresh_token})
+        return response
+
+    @staticmethod
+    def save_token_to_file(token):
+        with open('strava_token.json', 'w') as out:
+            json.dump(token.json(), out)
+
+    @staticmethod
+    def read_token_from_file():
+        with open('strava_token.json', 'r') as file:
+            token = json.load(file)
+            return token
