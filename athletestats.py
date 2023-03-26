ed4517c94f20412139289a531f192b86c38d9363
Pestap <90579181+Pestap@users.noreply.github.com>
2023-03-26 23:33:16 +0200

    Only all rides, all swims and all runs fields in Athletestats

--- athletestats.py
+++ athletestats.py
@@ -1,4 +1,12 @@
 
 class AthleteStats:
     def __init__(self, json: str):
-        self.stats = json
+        self.all_rides = json['all_ride_totals']
+        self.all_runs = json['all_run_totals']
+        self.all_swims = json['all_swim_totals']
+        self.longest_ride = json['biggest_ride_distance']
+
+    def __str__(self):
+        return  f"All rides: {self.all_rides}" \
+                f"All runs: {self.all_runs}" \
+                f"All swims: {self.all_swims}"
