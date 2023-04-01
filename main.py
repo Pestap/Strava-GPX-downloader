94261eaced938c81afa1b36305a4fa6c137056fc
Pestap <90579181+Pestap@users.noreply.github.com>
2023-04-01 23:25:19 +0200

    Modified main.py to test activity filtering

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from stravaathleterequests import StravaAthleteRequest
 
 request = StravaAthleteRequest()
-request.get_current_athlete()
-print(request.athlete)
+#request.get_current_athlete()
+#print(request.athlete)
 
-result = request.get_athlete_activities()
-print(result)
-print(len(result))
+#result = request.get_athlete_activities()
+
+request.get_activities_of_type("ride")
